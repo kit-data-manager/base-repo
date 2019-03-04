ee44e8f5d1426b265187f7adec6fdf79a586ad2d
Thomas Jejkal <thomas.jejkal@kit.edu>
2019-03-04 09:16:53 +0100

    Added build of generic-message-consumer to build script and travis build

--- build-with-dependencies.sh
+++ build-with-dependencies.sh
@@ -6,5 +6,8 @@ echo "Building service-base library"
 cd libraries/service-base 
 ./gradlew clean check install
 cd ../../
+cd libraries/generic-message-consumer
+./gradlew clean check install
+cd ../../
 echo "Building project and executing tests"
 ./gradlew -Ptravis clean check jacocoTestReport

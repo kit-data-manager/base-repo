01ee99d89ee90f51ba9848eb31e97aaaa8d79638
Thomas Jejkal <thomas.jejkal@kit.edu>
2019-04-16 14:04:17 +0200

    Check for audit test

--- src/test/java/edu/kit/datamanager/repo/test/integration/DataResourceControllerTest.java
+++ src/test/java/edu/kit/datamanager/repo/test/integration/DataResourceControllerTest.java
@@ -1497,9 +1497,9 @@ public class DataResourceControllerTest{
             "Bearer " + userToken)).andDo(print()).andExpect(status().isNotFound());
 
     //get version information
+    System.out.println("GET VERSION INFO");
     this.mockMvc.perform(get("/api/v1/dataresources/" + resourceId).header(HttpHeaders.AUTHORIZATION,
-            "Bearer " + userToken).header("Content-Type", "application/vnd.datamanager.audit+json")).andDo(print()).andExpect(status().isOk()).andExpect(MockMvcResultMatchers.header().exists("Version")).andExpect(MockMvcResultMatchers.header().string("Version", "2"));  }
-  
-  
-
+            "Bearer " + userToken).header("Content-Type", "application/vnd.datamanager.audit+json")).andDo(print()).andExpect(status().isOk()).andExpect(MockMvcResultMatchers.header().exists("Version")).andExpect(MockMvcResultMatchers.header().string("Version", "2"));
+    System.out.println("DONE!");
+  }
 }
